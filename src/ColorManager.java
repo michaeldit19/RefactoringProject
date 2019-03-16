d9204c692f067b7db9e59aaaddf560a4405f8177
Your name <you@example.com>
2019-03-16 21:29:48 +0000

    added setToWhite to colors class

--- src/ColorManager.java
+++ src/ColorManager.java
@@ -1,7 +1,36 @@
 import java.awt.Color;
 
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
 public class ColorManager {
+	
+	private JComboBox<String> genderCombo, departmentCombo, fullTimeCombo;
+	private JTextField idField, ppsField, surnameField, firstNameField, salaryField;
+	
+	public ColorManager(JTextField ppsField, JTextField surnameField, JTextField firstNameField, JTextField salaryField,
+			JComboBox<String> genderCombo, JComboBox<String> departmentCombo, JComboBox<String> fullTimeCombo) {
+		this.ppsField = ppsField;
+		this.surnameField = surnameField;
+		this.firstNameField = firstNameField;
+		this.salaryField = salaryField;
+		this.genderCombo = genderCombo;
+		this.departmentCombo = departmentCombo;
+		this.fullTimeCombo = fullTimeCombo;
+		
+	}
 
 	static Color colour1 = new Color(255, 150, 150);
 	
+	// set text field background colour to white
+		void setToWhite() {
+			ppsField.setBackground(UIManager.getColor("TextField.background"));
+			surnameField.setBackground(UIManager.getColor("TextField.background"));
+			firstNameField.setBackground(UIManager.getColor("TextField.background"));
+			salaryField.setBackground(UIManager.getColor("TextField.background"));
+			genderCombo.setBackground(UIManager.getColor("TextField.background"));
+			departmentCombo.setBackground(UIManager.getColor("TextField.background"));
+			fullTimeCombo.setBackground(UIManager.getColor("TextField.background"));
+		}// end setToWhite
 }
